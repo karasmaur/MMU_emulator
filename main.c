2824910d6e90faba77f83db38846ed66cfd00707
Mauricio <karasmauricio@gmail.com>
2019-06-21 16:06:41 -0300

    Create process function.

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#
+#include <pthread.h>
+
+void *process(){
+    // Creates the process and uses the secondary_memory to store the data, each position of the array it's a page.
+    // Randomly the process will request a page from the MMU so it can execute from memory.
+}
 
 int main() {
     char virtual_memory[128]; // 1 MB > 128 pages
