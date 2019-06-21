c481f79bbb862b3fb89680fab17b0d5d38ed424b
karas <karas@debian-mk.karas>
2019-06-21 16:00:25 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#
+
+int main() {
+    char virtual_memory[128]; // 1 MB > 128 pages
+    char physical_memory[8]; // 64k > 8 pgs
+    char secondary_memory[120];
+
+
+
+
+    return 0;
+}
