4e05f5d5dfff982eb740bce3ba1d5ccfdb5077f0
Mauricio <karasmauricio@gmail.com>
2019-06-21 16:20:11 -0300

    Create MMU function and instantiation of process.

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *process(){
-    // Creates the process and uses the secondary_memory to store the data, each position of the array it's a page.
-    // Randomly the process will request a page from the MMU so it can execute from memory.
+// Global variables:
+char virtual_memory[128]; // 1 MB > 128 pages
+char physical_memory[8]; // 64k > 8 pgs
+char secondary_memory[120];
+
+// Functions:
+void mmu(int pid, int pages){
+
 }
 
+void *process(void *id){
+    // Creates the process and uses the secondary_memory to store the data, each position of the array it's a page.
+    // Randomly the process will request a page from the MMU so it can execute from memory.
+    int pages = 12;
+    int pid = (int *)id;
+
+    mmu(pid, pages);
+
+}
+
+// Main:
 int main() {
-    char virtual_memory[128]; // 1 MB > 128 pages
-    char physical_memory[8]; // 64k > 8 pgs
-    char secondary_memory[120];
+    pthread_t processes[2];
+    int pid[2];
 
+    pid[0] = 1; // Setting pid of the first process to 1.
 
+    (void) pthread_create(&processes[0], NULL, process, NULL);
 
+    (void) pthread_join(processes[0], NULL);
 
     return 0;
 }
