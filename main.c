8b90d6eacf12d145d0be40c51a15aa228568a84e
mkaras <mkaras@atlassian.com>
2019-06-26 12:18:41 -0300

    Created move_page function.

--- main.c
+++ main.c
@@ -4,15 +4,17 @@
 #include <time.h>
 #include <stdlib.h>
 
-
 #define VIRTUAL_SIZE 128
 #define PHYSICAL_SIZE 8
 #define SECONDARY_SIZE 120
+#define RELOCATION_REGISTER 14000
 
 // Global variables:
 int virtual_memory[VIRTUAL_SIZE][2]; // 1 MB > 128 pages
-int physical_memory[PHYSICAL_SIZE][2]; // 64k > 8 pgs
+int physical_memory[PHYSICAL_SIZE][3]; // 64k > 8 pgs
 int secondary_memory[SECONDARY_SIZE][2];
+
+int indexing_table[VIRTUAL_SIZE][VIRTUAL_SIZE];
 
 // Functions:
 void mmu_create(int pid, int pages){
@@ -33,7 +35,7 @@ void mmu_create(int pid, int pages){
         for (int j = 0; j < SECONDARY_SIZE; j++) {
             if(secondary_memory[j][0] == NULL){
                 secondary_memory[j][0] = pid; // Process ID
-                secondary_memory[j][1] = i; // Page ID
+                secondary_memory[j][1] = i; // Page ID  -- + RELOCATION_REGISTER
                 break;
             }
         }
@@ -51,18 +53,42 @@ int mmu_load(int pid, int page){
     }
 
     // If page isn't in the physical memory, then it has to load it on it.
-    for (int j = 0; j < SECONDARY_SIZE; ++j) {
-        if(secondary_memory[i][0] == pid && secondary_memory[i][1]){
-
+    for (int i = 0; i < SECONDARY_SIZE; ++i) {
+        if(secondary_memory[i][0] == pid && secondary_memory[i][1] == page){
+            //Checks if the physical memory has space to accomodate the page.
+            for (int j = 0; j < PHYSICAL_SIZE; ++j) {
+                if(physical_memory[i][0] == NULL){
+                    return  move_page(j, i);
+                } else {
+                    int empty_space = find_victim();
+                    return move_page(empty_space, i);
+                }
+            }
         } else {
             printf("Page %d wasn't found in secondary memory!\n", page);
+            return 0;
         }
-
     }
 }
 
+int move_page(int primary, int secondary){
+    physical_memory[primary][0] = secondary_memory[secondary][0];
+    physical_memory[primary][1] = secondary_memory[secondary][2];
+    secondary_memory[secondary][0] = 0;
+    secondary_memory[secondary][1] = 0;
+
+    printf("Page moved to physical memory!\n");
+
+    return  physical_memory[primary][1];
+}
+
+int find_victim(){
+    return 0;
+}
+
 int *process(void *id) {
-    // Creates the process and uses the secondary_memory to store the data, each position of the array it's a page.
+    // Creates the process and uses the secondary_memory to store the data, each position of
+    // the array it's a page.
     // Randomly the process will request a page from the MMU so it can execute from memory.
     int pages = 12;
     //int pid = *((int *) id); // TODO: this is causing segmentation fault, SOLVE THIS!
@@ -78,6 +104,7 @@ int *process(void *id) {
 
         int random_page = rand() % pages;
 
+        printf("Requesting page %d\n", random_page);
         mmu_load(1, random_page);
     }
 
