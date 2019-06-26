e44bad914becd185f4f136e7c432cd6d3278cec2
mkaras <mkaras@atlassian.com>
2019-06-26 19:35:27 -0300

    Correct the segment fault issue

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define TOTAL_PROCESSES 2
 #define VIRTUAL_SIZE 128
 #define PHYSICAL_SIZE 8
 #define SECONDARY_SIZE 120
@@ -19,8 +20,10 @@
 int virtual_memory[VIRTUAL_SIZE][2]; // 1 MB > 128 pages
 int physical_memory[PHYSICAL_SIZE][3]; // 64k > 8 pgs
 int secondary_memory[SECONDARY_SIZE][2];
-
 int indexing_table[VIRTUAL_SIZE][3]; // Contains the translation of addresses from virtual to physical memory.
+
+pthread_mutex_t mutex;
+pthread_cond_t control;
 
 // Functions:
 void mmu_create_pages(int pid, int pages){
@@ -59,6 +62,7 @@ int translate_page_address(int pid, int page){
             return indexing_table[i][2];
         }
     }
+    return 0;
 }
 
 int mmu_load(int pid, int page){
@@ -69,17 +73,17 @@ int mmu_load(int pid, int page){
     for (int i = 0; i < PHYSICAL_SIZE; ++i) {
         if(physical_memory[i][0] == pid && physical_memory[i][1] == page){
             //Found page on physical memory
-            printf("Found page on physical memory: %d\n", physical_memory[i][1]);
+            printf("PID: %d, Found page on physical memory: %d\n", pid, physical_memory[i][1]);
             physical_memory[i][2]++; // Increments the use count.
             return 1;
         }
     }
 
-    printf("Page is not in physical memory!\n");
+    printf("PID: %d, Page %d is not in physical memory!\n", pid, page);
     // If page isn't in the physical memory, then it has to load it on it.
     for (int i = 0; i < SECONDARY_SIZE; ++i) {
         if(secondary_memory[i][0] == pid && secondary_memory[i][1] == page){
-            //Checks if the physical memory has space to accomodate the page.
+            //Checks if the physical memory has space to accommodate the page.
             for (int j = 0; j < PHYSICAL_SIZE; j++) {
                 if(physical_memory[j][0] == 0){
                     swap_in_mem(j, i);
@@ -93,7 +97,7 @@ int mmu_load(int pid, int page){
         }
     }
 
-    printf("Page %d wasn't found in secondary memory!\n", page);
+    printf("PID: %d, Page %d wasn't found in secondary memory!\n", pid, page);
     return 0;
 }
 
@@ -101,12 +105,13 @@ int swap_in_mem(int prim_position, int secon_position){
     // Swaps in pages from secondary memory into the physical primary memory.
     physical_memory[prim_position][0] = secondary_memory[secon_position][0]; // PID
     physical_memory[prim_position][1] = secondary_memory[secon_position][1]; // Page
-    physical_memory[prim_position][2] = 0; // Last used counter
+    physical_memory[prim_position][2] = 0; // Uses counter
 
     secondary_memory[secon_position][0] = 0; // PID
     secondary_memory[secon_position][1] = 0; // Page
 
-    printf("Page moved to physical memory!\n");
+    printf("PID: %d, Page %d moved to physical memory!\n", physical_memory[prim_position][0],
+           physical_memory[prim_position][1]);
 
     return  physical_memory[prim_position][1];
 }
@@ -117,35 +122,39 @@ int swap_out_mem(int prim_position, int secon_position){
     secondary_memory[secon_position][1] = physical_memory[prim_position][1]; // Page
     physical_memory[prim_position][0] = 0; // PID
     physical_memory[prim_position][1] = 0; // Page
-    physical_memory[prim_position][2] = 0;  // Last used counter
+    physical_memory[prim_position][2] = 0;  // Uses counter
 
     return secondary_memory[secon_position][1];
 }
 
 int find_victim(){
-    // TODO: Add mutex in here to avoid race condition.
-    int last_used_page[2];
-    last_used_page[0] = 0;
-    last_used_page[1] = 0;
+    pthread_mutex_lock(&mutex);
+
+    int most_unused_page[2];
+    most_unused_page[0] = physical_memory[0][0];
+    most_unused_page[1] = physical_memory[0][2];
 
     for (int i = 0; i < PHYSICAL_SIZE; ++i) {
-        if(physical_memory[i][2] > last_used_page[1]){
-            last_used_page[0] = i;
-            last_used_page[1] = physical_memory[i][2];
+        if(physical_memory[i][2] < most_unused_page[1]){
+            most_unused_page[0] = i;
+            most_unused_page[1] = physical_memory[i][2];
         }
     }
 
-    printf("Found Victim to move out of memory: PID: %d, Page: %d, Last Used: %d\n",
-           physical_memory[last_used_page[0]][0],  physical_memory[last_used_page[0]][1],
-           last_used_page[1]);
+    printf("Found Victim to move out of memory: PID: %d, Page: %d, Uses: %d\n",
+           physical_memory[most_unused_page[0]][0],  physical_memory[most_unused_page[0]][1],
+           most_unused_page[1]);
 
     for (int i = 0; i < SECONDARY_SIZE; i++) {
         if(secondary_memory[i][0] == 0){
-            swap_out_mem(last_used_page[0], i);
+            swap_out_mem(most_unused_page[0], i);
         }
     }
 
-    return last_used_page[0];
+    pthread_cond_signal(&control);
+    pthread_mutex_unlock(&mutex);
+
+    return most_unused_page[0];
 }
 
 int *process(void *id) {
@@ -153,10 +162,9 @@ int *process(void *id) {
     // the array it's a page.
     // Randomly the process will request a page from the MMU so it can execute from memory.
     int pages = 12;
-    //int pid = *((int *) id); // TODO: this is causing segmentation fault, SOLVE THIS!
-    //printf("%d\n", pid);
+    int pid = *((int *) id);
 
-    mmu_create_pages(1, pages);
+    mmu_create_pages(pid, pages);
 
     srand(time(NULL));
 
@@ -166,8 +174,8 @@ int *process(void *id) {
 
         int random_page = rand() % pages;
 
-        printf("Requesting page %d\n", random_page);
-        mmu_load(1, random_page);
+        printf("PID: %d, Requesting page %d\n", pid, random_page);
+        mmu_load(pid, random_page);
     }
 
     return 0;
@@ -175,12 +183,19 @@ int *process(void *id) {
 
 // Main:
 int main() {
-    pthread_t processes[2];
-    int pid[2];
+    pthread_t processes[TOTAL_PROCESSES];
 
-    pid[0] = 1; // Setting pid of the first process to 1.
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&control, NULL);
 
-    (void) pthread_create(&processes[0], NULL, process, (void*) pid[0]);
+    int *pid = malloc(sizeof(int)*TOTAL_PROCESSES);
+    for (int i = 0; i < TOTAL_PROCESSES; ++i) {
+        pid[i] = i+1;
+    }
+
+    for (int i = 0; i < TOTAL_PROCESSES; ++i) {
+        (void) pthread_create(&processes[i], NULL, process, (void *) &pid[i]);
+    }
 
     // Requests the pages.
     while(1) {
